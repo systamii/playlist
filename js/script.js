4cd95027bcd8d28608412a6a624234663320325b
Glitch (synonymous-wide-sponge) <none>
2022-02-10 23:20:30 +0000

    🕍👓 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // Complete Day 1 goals here
-
+class songs{
+  constructor(title, artist, image, link){
+    this.title = title;
+    this.artist = artist;
+    this.image = image;
+    this.link = link;
+  }
+  
+  
+}
+let songArray = ["Miss Cleo", "Mission To the Loot", "Love Sosa", "Vamp Anthem", "DRACO", "Sky", "Homecoming", "Jesus Walks", "Touch the Sky", "Good Morning", "HORRIBLE!", "Maze", "Who Shot Cupid?"]
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
 }
@@ -20,7 +30,7 @@ function addSongInfo() {
   // Complete Day 3 goals inside this function
 }
 
-$("#add").click(function () {
+$(".add").click(function () {
   emptySongInfo();
   addSongInfo();
   displaySongInfo();
